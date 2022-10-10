94f94785551e3084f4b2bffe694cb38ba5f90274
Sahil Sunil Phadtare <97933138+IamsahilPhadtare@users.noreply.github.com>
2022-10-10 21:56:52 +0530

    added login frame

---
+++ func.js
@@ -0,0 +1,8 @@
+function showpass() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
